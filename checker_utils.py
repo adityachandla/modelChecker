c7b1b425769ddcf1d8cba0b0fc6d7eeed89ae89d
adityachandla <adityachandla@gmail.com>
2023-12-07 12:00:51 +0100

    Added timing information

--- checker_utils.py
+++ checker_utils.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
+import time
 
 @dataclass
 class CheckerOutput:
     satisfied_states: set[int]
     num_iter: dict[str,int] # Number of iterations made on a variable
+    running_time_millis: int
+
+def get_time() -> int:
+    return round(time.time() * 1000)
