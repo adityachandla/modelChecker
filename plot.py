d904d67c1f98d1f2dbb2ce8bdb3ef4bc4ca5f1b3
edwinsteenkamer <36700301+edwinsteenkamer@users.noreply.github.com>
2023-12-13 09:26:12 +0100

    Fixed minor bug in plotting and added file save for plots

--- plot.py
+++ plot.py
@@ -10,6 +10,7 @@ from emerson import EmersonChecker
 
 def plot_num_iterations(experiment: str, xlabel: str, begin_range: int, end_range: int):
     emerson, naive = get_num_iterations(experiment)
+    i = 0
     for query_string, emerson_data in emerson.items():
         naive_data = naive[query_string]
         plt.figure()
@@ -21,14 +22,15 @@ def plot_num_iterations(experiment: str, xlabel: str, begin_range: int, end_rang
         plt.plot(x, emerson_data ,marker='o', label='Emerson')
         plt.plot(x, naive_data, marker='x', label='Naive')
         plt.legend()
-        plt.show()
+        plt.savefig(f"plots/{experiment}/{query_string}.png")
+        i += 1
 
 
 def get_num_iterations(experiment:str):
     path = f'Experiments/{experiment}/'
     query_files, graph_files = get_files(path)
-    print(graph_files)
     sorted_graph_files = sorted(graph_files, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+    print(query_files)
     queries = []
 
     emerson_iterations = {}
@@ -37,32 +39,34 @@ def get_num_iterations(experiment:str):
     for query_file in query_files:
         key = query.parse_query(path + query_file)
         queries.append(key)
-        emerson_iterations[key[2]] = []
-        naive_iterations[key[2]] = []
+        emerson_iterations[query_file] = []
+        naive_iterations[query_file] = []
     for graph_file in sorted_graph_files:
         print(f'hey {graph_file}')
         graph = Graph.from_file(path + graph_file)
-        checker = EmersonChecker(graph)
         for i in range(len(queries)):
+            checker = EmersonChecker(graph)
             formula, variables, formula_string = queries[i]
             res = checker.solve_formula(variables, formula)
             total_num_iter = 0
             for variable in res.num_iter:
                 total_num_iter += res.num_iter[variable]
-            emerson_iterations[formula_string] += [total_num_iter]
+            emerson_iterations[query_files[i]] += [total_num_iter]
 
             checker = NaiveChecker(graph)
             res = checker.solve_formula(variables, formula)
             total_num_iter = 0
             for variable in res.num_iter:
                 total_num_iter += res.num_iter[variable]
-            naive_iterations[formula_string] += [total_num_iter]
+            naive_iterations[query_files[i]] += [total_num_iter]
             print(f'query {i} done :)')
 
     return emerson_iterations, naive_iterations
 
 
-plot_num_iterations('dining', 'amount of professors', 2, 11)
+plot_num_iterations('demanding', 'amount of children', 2, 10)
+#plot_num_iterations('dining', 'amount of professors', 2, 11)
+#plot_num_iterations('ccp', 'amount of clients', 2, 5)
 
 
 
